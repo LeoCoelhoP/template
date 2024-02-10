90a9038e0fe515378ffc24ab748bca7001d569db
LeoCoelhoP <148665981+LeoCoelhoP@users.noreply.github.com>
2024-02-10 11:54:59 -0300

    Default template

---
+++ dist/bundle.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/***/ (() => {
+
+eval("console.log('leo');\n\nfunction leo() {\n  return 'leo';\n}\n\nvar leo1 = 12;\nconsole.log(leo());\n\nalert(leo1);\n\n\n//# sourceURL=webpack://template/./src/js/main.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/js/main.js"]();
+/******/ 	
+/******/ })()
+;
---
+++ src/js/main.js
@@ -0,0 +1,10 @@
+console.log('leo');
+
+function leo() {
+  return 'leo';
+}
+
+var leo1 = 12;
+console.log(leo());
+
+alert(leo1);
